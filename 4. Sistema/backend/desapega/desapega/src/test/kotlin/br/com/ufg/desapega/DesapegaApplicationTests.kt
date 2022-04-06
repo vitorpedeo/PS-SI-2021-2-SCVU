48cb43a60eff7ddfb3b5c7f9229a75aadff50428
Pedro Vinícius <pedrovovp@hotmail.com>
2022-04-05 23:14:42 -0300

    initial commit

---
+++ 4. Sistema/backend/desapega/desapega/src/test/kotlin/br/com/ufg/desapega/DesapegaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ufg.desapega
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesapegaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
---
+++ 4. Sistema/backend/desapega/src/main/kotlin/br/com/ufg/desapega/DesapegaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.ufg.desapega
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesapegaApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesapegaApplication>(*args)
+}
